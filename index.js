f8deffe84a6840d1e3b048277f4f2e853ced191b
Quentin Da Silva <32438087+Sh0ri@users.noreply.github.com>
2018-03-15 13:44:23 +0100

    return all models + added package.json file

--- index.js
+++ index.js
@@ -15,10 +15,29 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/api/populate', (req, res) => {
 	console.log("API CALL POPULATE");
 
-	obj = get_brands(res);
+	test_function(res);
 });
 
-
+async function test_function(res){
+	var brands = await getBrands();
+	var models = brands.map(brand => getModels(brand));
+	var obj = [];
+	var compteur = 0;
+	pSettle(models).then(result=>{
+		result.forEach(function(elem){
+			if(elem.isFulfilled && elem.value.length > 0){
+				obj.push(elem.value);
+				console.log(elem.value);
+				compteur++;
+			}
+			
+		});
+		res.send(obj);
+		console.log("Nomber of objects : " + compteur);
+	})
+	
+	//res.send(obj);
+}
 
 async function get_brands (res) {
 	var brands = await getBrands();
