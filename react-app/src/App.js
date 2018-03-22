8b6ae619bb120e7b322352a48c13c45b0410fc0a
Quentin Da Silva <32438087+Sh0ri@users.noreply.github.com>
2018-03-22 14:59:09 +0100

    visual done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -19,28 +19,60 @@ class App extends Component {
   componentDidMount() {
     fetch(API + DEFAULT_QUERY)
     .then(response => response.json())
-    .then(data => this.setState({ data: data }))
-    .then(()=>{console.log(this.state)});
+    .then(data => this.setState({ data: data }));
+  }
+
+  Go_To_Href(url){
+    window.open(url);
+  }
+
+  OrderBy(item){
+    var temp_data = this.state.data;
+    console.log(temp_data);
+    if(item==='brand')
+      temp_data.sort((a,b) => a.brand.localeCompare(b.brand));
+    if(item==='model')
+      temp_data.sort((a,b) => a.model.localeCompare(b.model));
+    if(item==='name')
+      temp_data.sort((a,b) => a.name.localeCompare(b.name));
+    if(item==='volume')
+      temp_data.sort((a,b) => a.volume.localeCompare(b.volume));
+
+    this.setState({ data: temp_data });
   }
 
 
   render() {
-    var reg =/[ ]/g;
     const { data } = this.state;
     return (
 
 
 
 
-      <div id='root' className="row">
-      <div class="list-group col-sm-6" id="root" className= "App">
+      <div id='root' className="container" style={{padding:'160px'}}>
+
+      <table className="table table-hover table-bordered table-hover-cells">
+      <thead>
+      <tr>
+      <th scope="col-sm-auto" onClick={()=>{this.OrderBy('brand')}}>Brand</th>
+      <th scope="col-sm-auto" onClick={()=>{this.OrderBy('model')}}>Model</th>
+      <th scope="col-sm-auto" onClick={()=>{this.OrderBy('name')}}>Name</th>
+      <th scope="col-sm-auto" onClick={()=>{this.OrderBy('volume')}}>Volume</th>
+      </tr>
+      </thead>
+      <tbody>
       {data.map(object =>
         //FOREACH
-        <a  key={object.uuid} className="list-group-item list-group-item-action col-sm-8" href={object.url}>
-        <h1>{object.name}</h1>
-        </a>
+        <tr class='clickable-row' href={object.url} onClick={()=>{this.Go_To_Href(object.url)}}>
+        <th scope="row">{object.brand}</th>
+        <td>{object.model}</td>
+        <td>{object.name}</td>
+        <td>{object.volume.length > 0 ? object.volume : '???'}</td>
+        </tr>
         )}
-      </div>
+      </tbody>
+
+      </table>
       </div>
       );
   }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Navbar from './Navbar';
 
+ReactDOM.render(<Navbar />, document.getElementById('navbar'));
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
